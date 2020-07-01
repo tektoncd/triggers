60f28427bc21ae43ae33acb066ff8675cf75c571
Kevin McDermott <bigkevmcd@gmail.com>
2020-07-01 17:11:57 +0100

    Fix issue with list results.
    There was a bug in the handling of list results, where they weren't being marshaled correctly, resulting in an error.
    
    This change identifies list results, and marshals them accordingly.
    
    This also bumps the version of cel to the latest 0.5.1

--- vendor/github.com/google/cel-go/cel/options.go
+++ vendor/github.com/google/cel-go/cel/options.go
@@ -29,6 +29,20 @@ import (
 	exprpb "google.golang.org/genproto/googleapis/api/expr/v1alpha1"
 )
 
+// These constants beginning with "Feature" enable optional behavior in
+// the library.  See the documentation for each constant to see its
+// effects, compatibility restrictions, and standard conformance.
+const (
+	_ = iota
+
+	// Disallow heterogeneous aggregate (list, map) literals.
+	// Note, it is still possible to have heterogeneous aggregates when
+	// provided as variables to the expression, as well as via conversion
+	// of well-known dynamic types, or with unchecked expressions.
+	// Affects checking.  Provides a subset of standard behavior.
+	FeatureDisableDynamicAggregateLiterals
+)
+
 // EnvOption is a functional interface for configuring the environment.
 type EnvOption func(e *Env) (*Env, error)
 
@@ -36,9 +50,6 @@ type EnvOption func(e *Env) (*Env, error)
 //
 // Clearing macros will ensure CEL expressions can only contain linear evaluation paths, as
 // comprehensions such as `all` and `exists` are enabled only via macros.
-//
-// Note: This option is a no-op when used with ClearBuiltIns, and must be used before Macros
-// if used together.
 func ClearMacros() EnvOption {
 	return func(e *Env) (*Env, error) {
 		e.macros = parser.NoMacros
@@ -68,12 +79,24 @@ func CustomTypeProvider(provider ref.TypeProvider) EnvOption {
 
 // Declarations option extends the declaration set configured in the environment.
 //
-// Note: This option must be specified after ClearBuiltIns if both are used together.
+// Note: Declarations will by default be appended to the pre-existing declaration set configured
+// for the environment. The NewEnv call builds on top of the standard CEL declarations. For a
+// purely custom set of declarations use NewCustomEnv.
 func Declarations(decls ...*exprpb.Decl) EnvOption {
 	// TODO: provide an alternative means of specifying declarations that doesn't refer
 	// to the underlying proto implementations.
 	return func(e *Env) (*Env, error) {
 		e.declarations = append(e.declarations, decls...)
+		return e, nil
+	}
+}
+
+// Features sets the given feature flags.  See list of Feature constants above.
+func Features(flags ...int) EnvOption {
+	return func(e *Env) (*Env, error) {
+		for _, flag := range flags {
+			e.SetFeature(flag)
+		}
 		return e, nil
 	}
 }
@@ -85,15 +108,12 @@ func Declarations(decls ...*exprpb.Decl) EnvOption {
 // expression, as well as via conversion of well-known dynamic types, or with unchecked
 // expressions.
 func HomogeneousAggregateLiterals() EnvOption {
-	return func(e *Env) (*Env, error) {
-		e.enableDynamicAggregateLiterals = false
-		return e, nil
-	}
+	return Features(FeatureDisableDynamicAggregateLiterals)
 }
 
 // Macros option extends the macro set configured in the environment.
 //
-// Note: This option must be specified after ClearBuiltIns and/or ClearMacros if used together.
+// Note: This option must be specified after ClearMacros if used together.
 func Macros(macros ...parser.Macro) EnvOption {
 	return func(e *Env) (*Env, error) {
 		e.macros = append(e.macros, macros...)
