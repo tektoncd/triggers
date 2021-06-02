bedb80e1d52450fed9dad4877bb2d97009e47548
SM43 <smukhade@redhat.com>
2021-06-02 17:25:12 +0530

    Removes podTemplate field from EventListener
    This removes the deprecated podTemplate field from eventlistener spec.
    
    Signed-off-by: Shivam Mukhade <smukhade@redhat.com>

--- pkg/apis/triggers/v1alpha1/event_listener_defaults.go
+++ pkg/apis/triggers/v1alpha1/event_listener_defaults.go
@@ -44,26 +44,5 @@ func (el *EventListener) SetDefaults(ctx context.Context) {
 				}
 			}
 		}
-		el.Spec.updatePodTemplate()
-	}
-}
-
-func (spec *EventListenerSpec) updatePodTemplate() {
-	if spec.DeprecatedPodTemplate != nil {
-		if spec.DeprecatedPodTemplate.NodeSelector != nil {
-			if spec.Resources.KubernetesResource == nil {
-				spec.Resources.KubernetesResource = &KubernetesResource{}
-			}
-			spec.Resources.KubernetesResource.Template.Spec.NodeSelector = spec.DeprecatedPodTemplate.NodeSelector
-			spec.DeprecatedPodTemplate.NodeSelector = nil
-		}
-		if spec.DeprecatedPodTemplate.Tolerations != nil {
-			if spec.Resources.KubernetesResource == nil {
-				spec.Resources.KubernetesResource = &KubernetesResource{}
-			}
-			spec.Resources.KubernetesResource.Template.Spec.Tolerations = spec.DeprecatedPodTemplate.Tolerations
-			spec.DeprecatedPodTemplate.Tolerations = nil
-		}
-		spec.DeprecatedPodTemplate = nil
 	}
 }
