59840e5b87362580ce481e475028e481a3e26f3b
sm43 <smukhade@redhat.com>
2021-07-19 21:48:37 +0530

    Improves e2e test for v1alpha1 examples with curl test
    This improves test by portforwading the el and then using curl
    command to test if a tr/pr is created successfully.
    
    Signed-off-by: Shivam Mukhade <smukhade@redhat.com>

---
+++ examples/v1alpha1/gitlab/curl.sh
@@ -0,0 +1,6 @@
+curl -v \
+-H 'X-GitLab-Token: 1234567' \
+-H 'X-Gitlab-Event: Push Hook' \
+-H 'Content-Type: application/json' \
+--data-binary "@examples/v1alpha1/gitlab/gitlab-push-event.json" \
+http://localhost:8080
--- test/e2e-tests-examples.sh
+++ test/e2e-tests-examples.sh
@@ -23,20 +23,21 @@ source $(dirname $0)/e2e-common.sh
 set -u -e -o pipefail -x
 
 current_example=""
+current_example_version=""
 yaml_files=""
+port_forward_pid=""
 
 info() {
-  echo; echo "*** Example ${current_example}: $@ ***"; echo;
+  echo; echo "*** Example ${current_example_version}/${current_example}: $@ ***"; echo;
 }
 
 err() {
-  echo; echo "ERROR: Example ${current_example}: $@"; echo;
+  echo; echo "ERROR: Example ${current_example_version}/${current_example}: $@"; echo;
 }
 
 apply_files() {
   info "Applying Resources"
-  local version=$1
-  folder=${REPO_ROOT_DIR}/examples/${version}/${current_example}
+  folder=${REPO_ROOT_DIR}/examples/${current_example_version}/${current_example}
   yaml_files=$(find ${folder}/ -name *.yaml | sort) || {
     err "failed to find files"
     exit 1
@@ -53,17 +54,76 @@ check_eventlistener() {
   info "Waiting for EventListener to be available"
 
   elName=${current_example}-listener
-  kubectl wait --for=condition=Ready --timeout=20s eventlisteners/${elName} || {
+  kubectl wait --for=condition=Ready --timeout=20s eventlisteners/${elName}  || {
     err "eventlistener failed to get in running state"
     exit 1
   }
   kubectl get eventlisteners
 }
 
+ignoreExamples=( cron trigger-ref v1alpha1-task )
+
+port_forward_and_curl() {
+
+   if [[ $version == "v1beta1" ]]; then
+    info "ignoring for now"
+  fi
+
+  if [[ " ${ignoreExamples[@]} " =~ " ${current_example} " ]]; then
+    info 'ignoring for port forwarding'
+    return
+  fi
+
+  info "Port forwarding to execute curl command"
+
+  kubectl port-forward service/el-${current_example}-listener 8080 &
+  port_forward_pid=$!
+
+  # Wait a few seconds for forwarding to happen
+  sleep 3
+
+  response=$(bash ${REPO_ROOT_DIR}/examples/${current_example_version}/${current_example}/curl.sh)
+
+  echo; echo "response received: $response"; echo
+
+  eventID=$(jq -n "$response" | jq --raw-output '.eventID')
+
+  if [ -z "$eventID" ]
+  then
+     err "failed to get eventID"
+     exit 1
+  fi
+
+  tr=$(kubectl get taskruns -A -l triggers.tekton.dev/triggers-eventid=${eventID} -o name)
+  pr=$(kubectl get pipelineruns -A -l triggers.tekton.dev/triggers-eventid=${eventID} -o name)
+
+  if [ -z "$tr" ] && [ -z "$pr" ]
+  then
+     err "failed to create taskrun/pipelinerun"
+     exit 1
+  fi
+
+  if [ -z "$tr" ]
+  then
+    echo "PipelineRun created : $pr"
+  else
+    echo "Taskrun created : $tr"
+  fi
+
+  kill $port_forward_pid
+}
+
+create_example_pipeline() {
+  kubectl apply -f ${REPO_ROOT_DIR}/examples/example-pipeline.yaml || {
+      err "failed to apply example pipeline"
+      exit 1
+  }
+}
+
 trap "cleanup" EXIT SIGINT
 cleanup() {
   info "Cleaning up resources"
-
+  kill $port_forward_pid || true
   for y in ${yaml_files}; do
     kubectl delete -f ${y} --ignore-not-found || true
   done
@@ -75,20 +135,23 @@ cleanup() {
 main() {
   versions="v1alpha1 v1beta1"
   # List of examples test will run on
-  examples="bitbucket cron embedded-trigger github gitlab label-selector namespace-selector trigger-ref v1alpha1-task"
-
+  examples="bitbucket cron embedded-trigger github gitlab label-selector namespace-selector v1alpha1-task trigger-ref"
+  create_example_pipeline
   for v in ${versions}; do
+    current_example_version=${v}
     echo "Applying examples for version: ${v}"
     for e in ${examples}; do
       current_example=${e}
-      echo "*** Example ${current_example} ***";
-      apply_files ${v}
+      echo "*** Example ${current_example_version}/${current_example} ***";
+      apply_files
       check_eventlistener
+      port_forward_and_curl
       cleanup
       info "Test Successful"
     done
   done
+
+  echo; echo "*** Completed Examples Test Successfully ***"; echo;
 }
 
 main $@
-
