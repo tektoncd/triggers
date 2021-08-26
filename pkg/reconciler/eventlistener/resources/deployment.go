49d1c7a0f541b23c0507afa140dc87cc52e7500c
Matt Moore <mattomata@gmail.com>
2021-08-26 15:05:01 -0700

    Add MakeDeployment as well.

---
+++ pkg/reconciler/eventlistener/resources/deployment.go
@@ -0,0 +1,236 @@
+/*
+Copyright 2021 The Tekton Authors
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package resources
+
+import (
+	"os"
+	"strconv"
+
+	"github.com/tektoncd/triggers/pkg/apis/triggers/v1beta1"
+	appsv1 "k8s.io/api/apps/v1"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/util/intstr"
+	"knative.dev/pkg/kmeta"
+	"knative.dev/pkg/metrics"
+	"knative.dev/pkg/ptr"
+)
+
+const (
+	// EventListenerConfigMapName is for the automatically created ConfigMap
+	EventListenerConfigMapName = "config-logging-triggers"
+
+	TriggersMetricsDomain = "tekton.dev/triggers"
+)
+
+var (
+	configLoggingVolume = corev1.Volume{
+		Name: "config-logging",
+		VolumeSource: corev1.VolumeSource{
+			ConfigMap: &corev1.ConfigMapVolumeSource{
+				LocalObjectReference: corev1.LocalObjectReference{
+					Name: EventListenerConfigMapName,
+				},
+			},
+		},
+	}
+	strongerSecurityPolicy = corev1.PodSecurityContext{
+		RunAsNonRoot: ptr.Bool(true),
+		RunAsUser:    ptr.Int64(65532),
+	}
+)
+
+func MakeDeployment(el *v1beta1.EventListener, c Config) (*appsv1.Deployment, error) {
+	opt, err := addDeploymentBits(el, c)
+	if err != nil {
+		return nil, err
+	}
+
+	container := MakeContainer(el, c, opt, addCertsForSecureConnection(c))
+
+	var (
+		replicas                  = ptr.Int32(1)
+		podlabels                 = kmeta.UnionMaps(el.Labels, GenerateLabels(el.Name, c.StaticResourceLabels))
+		serviceAccountName        = el.Spec.ServiceAccountName
+		vol                       = []corev1.Volume{configLoggingVolume}
+		tolerations               []corev1.Toleration
+		nodeSelector, annotations map[string]string
+	)
+
+	for _, v := range container.Env {
+		// If TLS related env are set then mount secret volume which will be used while starting the eventlistener.
+		if v.Name == "TLS_CERT" {
+			vol = append(vol, corev1.Volume{
+				Name: "https-connection",
+				VolumeSource: corev1.VolumeSource{
+					Secret: &corev1.SecretVolumeSource{
+						SecretName: v.ValueFrom.SecretKeyRef.Name,
+					},
+				},
+			})
+		}
+	}
+
+	if el.Spec.Resources.KubernetesResource != nil {
+		if el.Spec.Resources.KubernetesResource.Replicas != nil {
+			replicas = el.Spec.Resources.KubernetesResource.Replicas
+		}
+		if len(el.Spec.Resources.KubernetesResource.Template.Spec.Tolerations) != 0 {
+			tolerations = el.Spec.Resources.KubernetesResource.Template.Spec.Tolerations
+		}
+		if len(el.Spec.Resources.KubernetesResource.Template.Spec.NodeSelector) != 0 {
+			nodeSelector = el.Spec.Resources.KubernetesResource.Template.Spec.NodeSelector
+		}
+		if el.Spec.Resources.KubernetesResource.Template.Spec.ServiceAccountName != "" {
+			serviceAccountName = el.Spec.Resources.KubernetesResource.Template.Spec.ServiceAccountName
+		}
+		annotations = el.Spec.Resources.KubernetesResource.Template.Annotations
+		podlabels = kmeta.UnionMaps(podlabels, el.Spec.Resources.KubernetesResource.Template.Labels)
+	}
+
+	var securityContext corev1.PodSecurityContext
+	if *c.SetSecurityContext {
+		securityContext = strongerSecurityPolicy
+	}
+
+	return &appsv1.Deployment{
+		ObjectMeta: ObjectMeta(el, c.StaticResourceLabels),
+		Spec: appsv1.DeploymentSpec{
+			Replicas: replicas,
+			Selector: &metav1.LabelSelector{
+				MatchLabels: GenerateLabels(el.Name, c.StaticResourceLabels),
+			},
+			Template: corev1.PodTemplateSpec{
+				ObjectMeta: metav1.ObjectMeta{
+					Labels:      podlabels,
+					Annotations: annotations,
+				},
+				Spec: corev1.PodSpec{
+					Tolerations:        tolerations,
+					NodeSelector:       nodeSelector,
+					ServiceAccountName: serviceAccountName,
+					Containers:         []corev1.Container{container},
+					Volumes:            vol,
+					SecurityContext:    &securityContext,
+				},
+			},
+		},
+	}, nil
+}
+
+func addDeploymentBits(el *v1beta1.EventListener, c Config) (ContainerOption, error) {
+	// METRICS_PROMETHEUS_PORT defines the port exposed by the EventListener metrics endpoint
+	// env METRICS_PROMETHEUS_PORT set by controller
+	metricsPort, err := strconv.ParseInt(os.Getenv("METRICS_PROMETHEUS_PORT"), 10, 64)
+	if err != nil {
+		return nil, err
+	}
+
+	return func(container *corev1.Container) {
+		if el.Spec.Resources.KubernetesResource != nil {
+			if len(el.Spec.Resources.KubernetesResource.Template.Spec.Containers) != 0 {
+				container.Resources = el.Spec.Resources.KubernetesResource.Template.Spec.Containers[0].Resources
+				container.Env = append(container.Env, el.Spec.Resources.KubernetesResource.Template.Spec.Containers[0].Env...)
+			}
+		}
+
+		container.Ports = append(container.Ports, corev1.ContainerPort{
+			ContainerPort: int32(metricsPort),
+			Protocol:      corev1.ProtocolTCP,
+		})
+
+		// TODO(mattmoor): Knative's sharedmain no longer looks for this, so confirm this is still needed.
+		container.VolumeMounts = []corev1.VolumeMount{{
+			Name:      "config-logging",
+			MountPath: "/etc/config-logging",
+		}}
+
+		container.Env = append(container.Env, corev1.EnvVar{
+			Name: "SYSTEM_NAMESPACE",
+			ValueFrom: &corev1.EnvVarSource{
+				FieldRef: &corev1.ObjectFieldSelector{
+					FieldPath: "metadata.namespace",
+				}},
+		}, corev1.EnvVar{
+			Name:  "CONFIG_OBSERVABILITY_NAME",
+			Value: metrics.ConfigMapName(),
+		}, corev1.EnvVar{
+			Name:  "METRICS_DOMAIN",
+			Value: TriggersMetricsDomain,
+		}, corev1.EnvVar{
+			// METRICS_PROMETHEUS_PORT defines the port exposed by the EventListener metrics endpoint
+			// env METRICS_PROMETHEUS_PORT set by controller
+			Name:  "METRICS_PROMETHEUS_PORT",
+			Value: os.Getenv("METRICS_PROMETHEUS_PORT"),
+		})
+	}, nil
+}
+
+func addCertsForSecureConnection(c Config) ContainerOption {
+	return func(container *corev1.Container) {
+		var elCert, elKey string
+		certEnv := map[string]*corev1.EnvVarSource{}
+		for i := range container.Env {
+			certEnv[container.Env[i].Name] = container.Env[i].ValueFrom
+		}
+		var scheme corev1.URIScheme
+		if v, ok := certEnv["TLS_CERT"]; ok {
+			elCert = "/etc/triggers/tls/" + v.SecretKeyRef.Key
+		} else {
+			elCert = ""
+		}
+		if v, ok := certEnv["TLS_KEY"]; ok {
+			elKey = "/etc/triggers/tls/" + v.SecretKeyRef.Key
+		} else {
+			elKey = ""
+		}
+
+		if elCert != "" && elKey != "" {
+			scheme = corev1.URISchemeHTTPS
+			container.VolumeMounts = append(container.VolumeMounts, corev1.VolumeMount{
+				Name:      "https-connection",
+				ReadOnly:  true,
+				MountPath: "/etc/triggers/tls",
+			})
+		} else {
+			scheme = corev1.URISchemeHTTP
+		}
+		container.LivenessProbe = &corev1.Probe{
+			Handler: corev1.Handler{
+				HTTPGet: &corev1.HTTPGetAction{
+					Path:   "/live",
+					Scheme: scheme,
+					Port:   intstr.FromInt(eventListenerContainerPort),
+				},
+			},
+			PeriodSeconds:    int32(*c.PeriodSeconds),
+			FailureThreshold: int32(*c.FailureThreshold),
+		}
+		container.ReadinessProbe = &corev1.Probe{
+			Handler: corev1.Handler{
+				HTTPGet: &corev1.HTTPGetAction{
+					Path:   "/live",
+					Scheme: scheme,
+					Port:   intstr.FromInt(eventListenerContainerPort),
+				},
+			},
+			PeriodSeconds:    int32(*c.PeriodSeconds),
+			FailureThreshold: int32(*c.FailureThreshold),
+		}
+		container.Args = append(container.Args, "--tls-cert="+elCert, "--tls-key="+elKey)
+	}
+}
