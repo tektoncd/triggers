824ddda1f9b264388c08c9ec9e87ca2ce90bb0dc
savitaashture <sashture@redhat.com>
2021-10-25 21:44:39 +0530

    Fix script issue and update proper path for the examples

--- test/e2e-tests-yaml.sh
+++ test/e2e-tests-yaml.sh
@@ -4,39 +4,53 @@ set -x
 set -o errexit
 set -o pipefail
 
-for op in apply delete;do
-    # Apply TriggerTemplates
-    for file in $(find ${REPO_ROOT_DIR}/examples/triggertemplates/ -name *.yaml | sort); do
-        kubectl ${op} -f ${file}
-    done
+current_example=""
+current_example_version=""
 
-    # Apply TriggerBindings
-    for file in $(find ${REPO_ROOT_DIR}/examples/triggerbindings/ -name *.yaml | sort); do
-        kubectl ${op} -f ${file}
-    done
-
-    # Apply EventInterceptors
-    for file in $(find ${REPO_ROOT_DIR}/examples/event-interceptors/ -name *.yaml | sort); do
-        kubectl ${op} -f ${file}
-    done
-
-    # Apply EventListeners
-    for file in $(find ${REPO_ROOT_DIR}/examples/eventlisteners/ -name *.yaml | sort); do
-        kubectl ${op} -f ${file}
-    done
-
-    # Apply ClusterTriggerBindings
-    for file in $(find ${REPO_ROOT_DIR}/examples/clustertriggerbindings/ -name *.yaml | sort); do
-        kubectl ${op} -f ${file}
-    done
-
-done
-
-# Test getting-started examples that use the `getting-started` namespace
-kubectl create namespace getting-started
-for op in apply delete;do
-  for file in $(find ${REPO_ROOT_DIR}/docs/getting-started -name *.yaml | sort); do
-      kubectl ${op} -f ${file}
+apply_files() {
+  echo "Applying Resources"
+  yaml_files=$(find ${REPO_ROOT_DIR}/examples/${current_example_version}/${current_example} -type f \( -iname "*.yaml" ! -iname "rbac.yaml" \) | sort) || {
+    echo "failed to find files"
+    exit 1
+  }
+  for y in ${yaml_files}; do
+    kubectl apply  -f ${y} || {
+      echo "failed to apply ${y}"
+      exit 1
+    }
   done
-done
-kubectl delete namespace getting-started
+}
+
+delete_files() {
+  echo "Cleaning up resources"
+  for y in ${yaml_files}; do
+    kubectl delete -f ${y} --ignore-not-found || true
+  done
+}
+
+main() {
+  versions="v1alpha1 v1beta1"
+  examples="triggertemplates triggerbindings eventlisteners clustertriggerbindings"
+  for v in ${versions}; do
+    current_example_version=${v}
+    echo "Applying examples for version: ${v}"
+    for e in ${examples}; do
+      current_example=${e}
+      echo "*** Example ${current_example_version}/${current_example} ***";
+      apply_files
+      delete_files
+      echo "Test Successful"
+    done
+  done
+
+  # Test getting-started examples that use the `getting-started` namespace
+  kubectl create namespace getting-started
+  for op in apply delete;do
+    for file in $(find ${REPO_ROOT_DIR}/docs/getting-started -name *.yaml | sort); do
+        kubectl ${op} -f ${file}
+    done
+  done
+  kubectl delete namespace getting-started
+}
+
+main $@
