35afb454169ab9936bc0efdfdbe29e2a62a96077
Dibyo Mukherjee <dibyo@google.com>
2022-08-18 18:10:55 -0400

    Remove duplicate word

--- pkg/sink/validate_payload.go
+++ pkg/sink/validate_payload.go
@@ -37,7 +37,7 @@ func (r Sink) IsValidPayload(eventHandler http.Handler) http.Handler {
 		if r.PayloadValidation {
 			var event map[string]interface{}
 			if err := json.Unmarshal(payload, &event); err != nil {
-				errMsg := fmt.Sprintf("Invalid event body format format: %s", err)
+				errMsg := fmt.Sprintf("Invalid event body format : %s", err)
 				r.recordCountMetrics(failTag)
 				r.Logger.Error(errMsg)
 				response.WriteHeader(http.StatusBadRequest)
