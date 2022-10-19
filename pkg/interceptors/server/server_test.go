a9c50b16323ca906d5af4f0210658cb1cdfd55ce
savitaashture <sashture@redhat.com>
2022-10-19 19:15:07 +0530

    Refactor interceptor main package

--- pkg/interceptors/server/server_test.go
+++ pkg/interceptors/server/server_test.go
@@ -19,17 +19,27 @@ import (
 	faketriggersclient "github.com/tektoncd/triggers/pkg/client/injection/client/fake"
 	"github.com/tektoncd/triggers/pkg/interceptors"
 	"github.com/tektoncd/triggers/test"
+	"go.uber.org/zap"
 	"go.uber.org/zap/zaptest"
 	"google.golang.org/grpc/codes"
 	corev1 "k8s.io/api/core/v1"
+	apiErrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	v1 "k8s.io/client-go/kubernetes/typed/core/v1"
 	fakekubeclient "knative.dev/pkg/client/injection/kube/client/fake"
+	fakesecretinformer "knative.dev/pkg/client/injection/kube/informers/core/v1/secret"
 	"knative.dev/pkg/system"
+	certresources "knative.dev/pkg/webhook/certificates/resources"
 )
 
 const (
 	second = 6 * time.Second
+)
+
+var (
+	testsvc     = "testsvc"
+	testsecrets = "testsecrets"
+	testns      = "testns"
 )
 
 func TestServer_ServeHTTP(t *testing.T) {
@@ -198,20 +208,20 @@ func Test_SecretNotExist(t *testing.T) {
 	logger := zaptest.NewLogger(t)
 	ctx, _ := test.SetupFakeContext(t)
 	clientSet := fakekubeclient.Get(ctx).CoreV1()
-	_, _, err := CreateCerts(ctx, clientSet, time.Now().Add(Decade), logger.Sugar())
+	_, _, err := createCerts(ctx, clientSet, time.Now().Add(Decade), logger.Sugar())
 	if err != nil && !strings.Contains(err.Error(), "not found") {
 		t.Error(err)
 	}
 }
 
 func createSecret(t *testing.T, noAfter time.Time) (v1.CoreV1Interface, []byte, []byte, error) {
-	if err := os.Setenv(InterceptorTLSSvcKey, "testsvc"); err != nil {
+	if err := os.Setenv(interceptorTLSSvcKey, testsvc); err != nil {
 		return nil, []byte{}, []byte{}, err
 	}
-	if err := os.Setenv(InterceptorTLSSecretKey, "testsecrets"); err != nil {
+	if err := os.Setenv(interceptorTLSSecretKey, testsecrets); err != nil {
 		return nil, []byte{}, []byte{}, err
 	}
-	if err := os.Setenv("SYSTEM_NAMESPACE", "testns"); err != nil {
+	if err := os.Setenv("SYSTEM_NAMESPACE", testns); err != nil {
 		return nil, []byte{}, []byte{}, err
 	}
 	namespace := system.Namespace()
@@ -221,14 +231,14 @@ func createSecret(t *testing.T, noAfter time.Time) (v1.CoreV1Interface, []byte, 
 	clientSet := fakekubeclient.Get(ctx).CoreV1()
 	localObject := &corev1.Secret{
 		ObjectMeta: metav1.ObjectMeta{
-			Name:      os.Getenv(InterceptorTLSSecretKey),
+			Name:      os.Getenv(interceptorTLSSecretKey),
 			Namespace: namespace,
 		},
 	}
 	if _, err := clientSet.Secrets(namespace).Create(ctx, localObject, metav1.CreateOptions{}); err != nil {
 		t.Error(err)
 	}
-	sCert, caCert, err := CreateCerts(ctx, clientSet, noAfter, logger.Sugar())
+	sCert, caCert, err := createCerts(ctx, clientSet, noAfter, logger.Sugar())
 	return clientSet, sCert, caCert, err
 }
 
@@ -239,52 +249,6 @@ func Test_CreateSecret(t *testing.T) {
 	}
 	if len(sCert) == 0 && len(caCert) == 0 {
 		t.Error("expected serverCert and caCert to be created")
-	}
-}
-
-func Test_UpdateCRDWithCaCert(t *testing.T) {
-	ctx, _ := test.SetupFakeContext(t)
-	logger := zaptest.NewLogger(t)
-	_, sCert, caCert, err := createSecret(t, time.Now().Add(Decade))
-	if err != nil {
-		t.Error(err)
-	}
-	if len(sCert) == 0 && len(caCert) == 0 {
-		t.Error("expected serverCert and caCert to be created")
-	}
-	server, err := NewWithCoreInterceptors(interceptors.DefaultSecretGetter(fakekubeclient.Get(ctx).CoreV1()), logger.Sugar())
-	if err != nil {
-		t.Fatalf("error initializing core interceptors: %v", err)
-	}
-	server.RegisterInterceptor("firstci", fakeInterceptor{})
-	ci := v1alpha1.ClusterInterceptor{
-		ObjectMeta: metav1.ObjectMeta{
-			Name: "firstci",
-		},
-		Spec: v1alpha1.ClusterInterceptorSpec{
-			ClientConfig: v1alpha1.ClientConfig{
-				CaBundle: nil,
-				URL:      nil,
-				Service:  nil,
-			},
-		},
-	}
-	var ciList []v1alpha1.ClusterInterceptor
-	ciList = append(ciList, ci)
-	if _, err := faketriggersclient.Get(ctx).TriggersV1alpha1().ClusterInterceptors().Create(ctx, &ci, metav1.CreateOptions{}); err != nil {
-		t.Error(err)
-	}
-
-	if err := server.updateCRDWithCaCert(ctx, faketriggersclient.Get(ctx).TriggersV1alpha1(), ciList, caCert); err != nil {
-		t.Error(err)
-	}
-
-	ciNew, err := faketriggersclient.Get(ctx).TriggersV1alpha1().ClusterInterceptors().Get(ctx, "firstci", metav1.GetOptions{})
-	if err != nil {
-		t.Error(err)
-	}
-	if string(ciNew.Spec.ClientConfig.CaBundle) == "" {
-		t.Error("caBundle should exist after successful clusterinterceptor creation")
 	}
 }
 
@@ -300,25 +264,8 @@ func Test_CheckCertValidity(t *testing.T) {
 	}
 
 	tc := faketriggersclient.Get(ctx)
+	server, ci := registerAndGetCI(ctx, t, "firstciforgoroutine", logger)
 
-	server, err := NewWithCoreInterceptors(interceptors.DefaultSecretGetter(fakekubeclient.Get(ctx).CoreV1()), logger.Sugar())
-	if err != nil {
-		t.Fatalf("error initializing core interceptors: %v", err)
-	}
-	server.RegisterInterceptor("firstciforgoroutine", fakeInterceptor{})
-
-	ci := v1alpha1.ClusterInterceptor{
-		ObjectMeta: metav1.ObjectMeta{
-			Name: "firstciforgoroutine",
-		},
-		Spec: v1alpha1.ClusterInterceptorSpec{
-			ClientConfig: v1alpha1.ClientConfig{
-				CaBundle: nil,
-				URL:      nil,
-				Service:  nil,
-			},
-		},
-	}
 	var ciList []v1alpha1.ClusterInterceptor
 	ciList = append(ciList, ci)
 	if _, err := faketriggersclient.Get(ctx).TriggersV1alpha1().ClusterInterceptors().Create(ctx, &ci, metav1.CreateOptions{}); err != nil {
@@ -334,7 +281,7 @@ func Test_CheckCertValidity(t *testing.T) {
 		t.Error(err)
 	}
 
-	server.CheckCertValidity(ctx, sCert, caCert, clientSet, logger.Sugar(), tc.TriggersV1alpha1(), 1*time.Second)
+	server.checkCertValidity(ctx, sCert, caCert, clientSet, logger.Sugar(), tc.TriggersV1alpha1(), time.Second)
 
 	time.Sleep(10 * time.Second)
 	ciNew1, err := faketriggersclient.Get(ctx).TriggersV1alpha1().ClusterInterceptors().Get(ctx, "firstciforgoroutine", metav1.GetOptions{})
@@ -345,4 +292,208 @@ func Test_CheckCertValidity(t *testing.T) {
 	if string(ciNew1.Spec.ClientConfig.CaBundle) == "" || string(ciNew1.Spec.ClientConfig.CaBundle) == string(ciNew.Spec.ClientConfig.CaBundle) {
 		t.Error("timeout or failed to regenerate certificate after the certificate expire")
 	}
+}
+
+func Test_CreateAndValidateCerts(t *testing.T) {
+	ctx, _ := test.SetupFakeContext(t)
+	logger := zaptest.NewLogger(t)
+	clientSet := fakekubeclient.Get(ctx).CoreV1()
+	tc := faketriggersclient.Get(ctx)
+	if err := os.Setenv(interceptorTLSSecretKey, testsecrets); err != nil {
+		t.Error(err)
+	}
+
+	createSecretWithData(ctx, t, clientSet)
+
+	server, ci := registerAndGetCI(ctx, t, "firstci", logger)
+	if _, err := faketriggersclient.Get(ctx).TriggersV1alpha1().ClusterInterceptors().Create(ctx, &ci, metav1.CreateOptions{}); err != nil {
+		t.Error(err)
+	}
+
+	CreateAndValidateCerts(ctx, clientSet, logger.Sugar(), server, tc.TriggersV1alpha1())
+
+	ciNew, err := faketriggersclient.Get(ctx).TriggersV1alpha1().ClusterInterceptors().Get(ctx, "firstci", metav1.GetOptions{})
+	if err != nil {
+		t.Error(err)
+	}
+
+	if string(ciNew.Spec.ClientConfig.CaBundle) == "" {
+		t.Error("caBundle should exist after successful clusterinterceptor creation")
+	}
+}
+
+func Test_GetTLSData(t *testing.T) {
+	ctx, _ := test.SetupFakeContext(t)
+	logger := zaptest.NewLogger(t)
+	secretInformer := fakesecretinformer.Get(ctx)
+	clientSet := fakekubeclient.Get(ctx).CoreV1()
+	serverKey, serverCert, caCert, namespace := getCerts(ctx, t)
+	tests := []struct {
+		name       string
+		secretName string
+		secretData map[string][]byte
+		want       string
+	}{
+		{
+			name:       "Invalid secret",
+			secretName: "test111",
+			want:       "secret \"test111\" not found",
+		},
+		{
+			name:       "missing key",
+			secretName: testsecrets,
+			secretData: map[string][]byte{
+				certresources.ServerCert: serverCert,
+				certresources.CACert:     caCert,
+			},
+			want: "server key missing",
+		},
+		{
+			name:       "missing cert",
+			secretName: testsecrets,
+			secretData: map[string][]byte{
+				certresources.ServerKey: serverKey,
+				certresources.CACert:    caCert,
+			},
+			want: "server cert missing",
+		},
+		{
+			name:       "Invalid certs",
+			secretName: testsecrets,
+			secretData: map[string][]byte{},
+			want:       "server key missing",
+		},
+		{
+			name:       "Valid certs",
+			secretName: testsecrets,
+			secretData: map[string][]byte{
+				certresources.ServerKey:  serverKey,
+				certresources.ServerCert: serverCert,
+				certresources.CACert:     caCert,
+			},
+			want: "",
+		},
+	}
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			if err := os.Setenv(interceptorTLSSecretKey, tc.secretName); err != nil {
+				t.Error(err)
+			}
+			if err := clientSet.Secrets(namespace).Delete(ctx, tc.secretName, metav1.DeleteOptions{}); err != nil && !apiErrors.IsNotFound(err) {
+				t.Error(err)
+			}
+			localObject := &corev1.Secret{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      tc.secretName,
+					Namespace: namespace,
+				},
+				Data: tc.secretData,
+			}
+			// In order to test secret not found scenario
+			if os.Getenv(interceptorTLSSecretKey) == testsecrets {
+				s, err := clientSet.Secrets(namespace).Create(ctx, localObject, metav1.CreateOptions{})
+				if err != nil {
+					t.Error(err)
+				}
+				if err = secretInformer.Informer().GetIndexer().Add(s); err != nil {
+					t.Error(err)
+				}
+			}
+			if _, err := GetTLSData(ctx, logger.Sugar()); err != nil && err.Error() != tc.want {
+				t.Error(err)
+			}
+		})
+	}
+}
+
+func Test_UpdateCACertToClusterInterceptorCRD(t *testing.T) {
+	ctx, _ := test.SetupFakeContext(t)
+	logger := zaptest.NewLogger(t)
+	secretInformer := fakesecretinformer.Get(ctx)
+	clientSet := fakekubeclient.Get(ctx).CoreV1()
+	if err := os.Setenv(interceptorTLSSecretKey, testsecrets); err != nil {
+		t.Error(err)
+	}
+
+	s := createSecretWithData(ctx, t, clientSet)
+	if err := secretInformer.Informer().GetIndexer().Add(s); err != nil {
+		t.Error(err)
+	}
+
+	server, ci := registerAndGetCI(ctx, t, "firstci1", logger)
+	if _, err := faketriggersclient.Get(ctx).TriggersV1alpha1().ClusterInterceptors().Create(ctx, &ci, metav1.CreateOptions{}); err != nil {
+		t.Error(err)
+	}
+
+	UpdateCACertToClusterInterceptorCRD(ctx, server, faketriggersclient.Get(ctx).TriggersV1alpha1(), logger.Sugar(), time.Second)
+
+	time.Sleep(10 * time.Second)
+	ciNew, err := faketriggersclient.Get(ctx).TriggersV1alpha1().ClusterInterceptors().Get(ctx, "firstci1", metav1.GetOptions{})
+	if err != nil {
+		t.Error(err)
+	}
+
+	if string(ciNew.Spec.ClientConfig.CaBundle) == "" {
+		t.Error("caBundle should exist after successful clusterinterceptor creation")
+	}
+}
+
+func getCerts(ctx context.Context, t *testing.T) ([]byte, []byte, []byte, string) {
+	if err := os.Setenv(interceptorTLSSvcKey, testsvc); err != nil {
+		t.Error(err)
+	}
+	if err := os.Setenv("SYSTEM_NAMESPACE", testns); err != nil {
+		t.Error(err)
+	}
+	namespace := system.Namespace()
+
+	serverKey, serverCert, caCert, err := certresources.CreateCerts(ctx, os.Getenv(interceptorTLSSvcKey), namespace, time.Now().Add(second))
+	if err != nil {
+		t.Error(err)
+	}
+	return serverKey, serverCert, caCert, namespace
+}
+
+func registerAndGetCI(ctx context.Context, t *testing.T, ciName string, logger *zap.Logger) (*Server, v1alpha1.ClusterInterceptor) {
+	server, err := NewWithCoreInterceptors(interceptors.DefaultSecretGetter(fakekubeclient.Get(ctx).CoreV1()), logger.Sugar())
+	if err != nil {
+		t.Fatalf("error initializing core interceptors: %v", err)
+	}
+	server.RegisterInterceptor(ciName, fakeInterceptor{})
+	ci := v1alpha1.ClusterInterceptor{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: ciName,
+		},
+		Spec: v1alpha1.ClusterInterceptorSpec{
+			ClientConfig: v1alpha1.ClientConfig{
+				CaBundle: nil,
+				URL:      nil,
+				Service:  nil,
+			},
+		},
+	}
+	return server, ci
+}
+
+func createSecretWithData(ctx context.Context, t *testing.T, clientSet v1.CoreV1Interface) *corev1.Secret {
+	serverKey, serverCert, caCert, namespace := getCerts(ctx, t)
+	if len(serverCert) == 0 && len(caCert) == 0 && len(serverKey) == 0 {
+		t.Error("expected serverCert, caCert and serverKey to be created")
+	}
+	localObject := &corev1.Secret{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      os.Getenv(interceptorTLSSecretKey),
+			Namespace: namespace,
+		},
+		Data: map[string][]byte{
+			certresources.ServerKey:  serverKey,
+			certresources.ServerCert: serverCert,
+			certresources.CACert:     caCert,
+		},
+	}
+	s, err := clientSet.Secrets(namespace).Create(ctx, localObject, metav1.CreateOptions{})
+	if err != nil {
+		t.Error(err)
+	}
+	return s
 }
