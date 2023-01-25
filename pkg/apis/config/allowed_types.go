e4fa87ad765600ff7686032253bab1069ab6db06
Dibyo Mukherjee <dibyo@google.com>
2023-01-25 16:34:51 -0500

    Add support for v1 pipeline types and customruns
    Allow triggers to create v1 pipeline types
    Also allow triggers to create v1beta1 customruns
    Finally, move the allowlisted types to a single file (fixes #494)
    
    Signed-off-by: Dibyo Mukherjee <dibyo@google.com>

--- pkg/apis/config/allowed_types.go
+++ pkg/apis/config/allowed_types.go
@@ -1,6 +1,7 @@
 package config
 
 import (
+	pipelinev1 "github.com/tektoncd/pipeline/pkg/apis/pipeline/v1"
 	customrunsv1alpha1 "github.com/tektoncd/pipeline/pkg/apis/pipeline/v1alpha1"
 	pipelinev1beta1 "github.com/tektoncd/pipeline/pkg/apis/pipeline/v1beta1"
 	pipelineresourcev1alpha1 "github.com/tektoncd/pipeline/pkg/apis/resource/v1alpha1"
@@ -17,10 +18,12 @@ func init() {
 	utilruntime.Must(customrunsv1alpha1.AddToScheme(scheme))
 	utilruntime.Must(pipelineresourcev1alpha1.AddToScheme(scheme))
 	utilruntime.Must(pipelinev1beta1.AddToScheme(scheme))
+	utilruntime.Must(pipelinev1.AddToScheme(scheme))
 	codec := serializer.NewCodecFactory(scheme)
 	Decoder = codec.UniversalDecoder(
 		pipelineresourcev1alpha1.SchemeGroupVersion, // customrunsv1alpha1 share the same SchemeGroupVersion
 		pipelinev1beta1.SchemeGroupVersion,
+		pipelinev1.SchemeGroupVersion,
 	)
 }
 
@@ -30,3 +33,15 @@ func EnsureAllowedType(rt runtime.RawExtension) error {
 	_, err := runtime.Decode(Decoder, rt.Raw)
 	return err
 }
+
+var (
+	AllowedPipelineTypes = map[string][]string{
+		"v1alpha1": {"pipelineresources", "pipelineruns", "taskruns", "pipelines", "clustertasks", "tasks", "conditions", "runs"},
+		"v1beta1":  {"pipelineruns", "taskruns", "pipelines", "clustertasks", "tasks", "customruns"},
+		"v1":       {"pipelineruns", "taskruns", "pipelines", "tasks"},
+	}
+	AllowedTriggersTypes = map[string][]string{
+		"v1alpha1": {"clusterinterceptors", "interceptors"},
+		"v1beta1":  {"clustertriggerbindings", "eventlisteners", "triggerbindings", "triggers", "triggertemplates"},
+	}
+)
