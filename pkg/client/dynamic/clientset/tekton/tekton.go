494882ea1ffc09a31a30bad96d84d8d13117f6a1
Dibyo Mukherjee <dibyo@google.com>
2023-01-25 16:34:51 -0500

    Add support for v1 pipeline types and customruns
    Allow triggers to create v1 pipeline types
    Also allow triggers to create v1beta1 customruns
    Finally, move the allowlisted types to a single file (fixes #494)
    
    Signed-off-by: Dibyo Mukherjee <dibyo@google.com>

--- pkg/client/dynamic/clientset/tekton/tekton.go
+++ pkg/client/dynamic/clientset/tekton/tekton.go
@@ -18,27 +18,17 @@ package tekton
 
 import (
 	"github.com/tektoncd/pipeline/pkg/apis/pipeline"
+	"github.com/tektoncd/triggers/pkg/apis/config"
 	"github.com/tektoncd/triggers/pkg/apis/triggers"
 	"github.com/tektoncd/triggers/pkg/client/dynamic/clientset"
 	"k8s.io/apimachinery/pkg/runtime/schema"
 	"k8s.io/client-go/dynamic"
 )
 
-var (
-	allowedPipelineTypes = map[string][]string{
-		"v1alpha1": {"pipelineresources", "pipelineruns", "taskruns", "pipelines", "clustertasks", "tasks", "conditions", "runs"},
-		"v1beta1":  {"pipelineruns", "taskruns", "pipelines", "clustertasks", "tasks"},
-	}
-	allowedTriggersTypes = map[string][]string{
-		"v1alpha1": {"clusterinterceptors", "interceptors"},
-		"v1beta1":  {"clustertriggerbindings", "eventlisteners", "triggerbindings", "triggers", "triggertemplates"},
-	}
-)
-
 // WithClient adds Tekton related clients to the Dynamic client.
 func WithClient(client dynamic.Interface) clientset.Option {
 	return func(cs *clientset.Clientset) {
-		for version, resources := range allowedPipelineTypes {
+		for version, resources := range config.AllowedPipelineTypes {
 			for _, resource := range resources {
 				r := schema.GroupVersionResource{
 					Group:    pipeline.GroupName,
@@ -48,7 +38,7 @@ func WithClient(client dynamic.Interface) clientset.Option {
 				cs.Add(r, client)
 			}
 		}
-		for version, resources := range allowedTriggersTypes {
+		for version, resources := range config.AllowedTriggersTypes {
 			for _, resource := range resources {
 				r := schema.GroupVersionResource{
 					Group:    triggers.GroupName,
