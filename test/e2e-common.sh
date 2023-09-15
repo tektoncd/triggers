5cfc1a5d9232ff6cf72f8049a91d2f58dc4aa9a9
Khurram Baig <kbaig@redhat.com>
2023-09-15 19:11:01 +0530

    Remove Pipelineressources from integration test
    We get a false negative during Triggers integration testing.

--- test/e2e-common.sh
+++ test/e2e-common.sh
@@ -42,7 +42,7 @@ function install_pipeline_crd() {
     fail_test "Tekton pipeline installation failed"
 
   # Make sure that eveything is cleaned up in the current namespace.
-  for res in pipelineresources tasks pipelines taskruns pipelineruns; do
+  for res in tasks pipelines taskruns pipelineruns; do
     kubectl delete --ignore-not-found=true ${res}.tekton.dev --all
   done
 
