7772b35c9f6e2d8f2bd3bc9d675e45c00a9999a6
Khurram Baig <kbaig@redhat.com>
2025-04-08 17:22:08 +0530

    Update to latest pipeline and knative/pkg

--- vendor/github.com/prometheus/common/model/labelset_string.go
+++ vendor/github.com/prometheus/common/model/labelset_string.go
@@ -11,13 +11,11 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-//go:build go1.21
-
 package model
 
 import (
 	"bytes"
-	"sort"
+	"slices"
 	"strconv"
 )
 
@@ -28,7 +26,7 @@ func (l LabelSet) String() string {
 	for name := range l {
 		labelNames = append(labelNames, string(name))
 	}
-	sort.Strings(labelNames)
+	slices.Sort(labelNames)
 	var bytea [1024]byte // On stack to avoid memory allocation while building the output.
 	b := bytes.NewBuffer(bytea[:0])
 	b.WriteByte('{')
