1e498fddae55fadfbd7d0090949b499936a40554
Khurram Baig <kbaig@redhat.com>
2025-07-03 20:59:15 +0530

    Fix panic for e2e go tests
    Go test panic while running under microshift. This fixes that.

--- test/eventlistener_test.go
+++ test/eventlistener_test.go
@@ -493,9 +493,6 @@ func TestEventListenerCreate(t *testing.T) {
 		}
 	}
 
-	fmt.Printf("\nel.Spec.Resources.KubernetesResource.ServiceType: %v", el.Spec.Resources.KubernetesResource.ServiceType)
-	fmt.Printf("\nel.Spec.Resources.KubernetesResource.ServicePort: %v", *el.Spec.Resources.KubernetesResource.ServicePort)
-
 	// Send POST request to EventListener sink
 	var resp *http.Response
 	if err := WaitFor(func() (bool, error) {
