5f58ccbb0eadf6de7dd989212f18145fd010239c
Khurram Baig <kbaig@redhat.com>
2026-03-12 16:15:07 +0530

    Fix e2e failure due to owners file change
    The GitHub owner's example test is failing due to a change in owner.

--- examples/v1beta1/github-owners/curl.sh
+++ examples/v1beta1/github-owners/curl.sh
@@ -1,6 +1,6 @@
 curl -v \
 -H 'X-GitHub-Event: pull_request' \
--H 'X-Hub-Signature-256: sha256=a7b3a3840860ef271afde557e8b6c89cc69539a396417f93d847e1890d3c8184' \
+-H 'X-Hub-Signature-256: sha256=407de896d43ea3605e0e897112493c5f81eb052bff0b48783f7dfa264e3dceae' \
 -H 'Content-Type: application/json' \
--d '{"action": "opened","number": 1503,"repository":{"full_name": "tektoncd/triggers", "clone_url": "https://github.com/tektoncd/triggers.git"}, "sender":{"login": "dibyom"}}' \
+-d '{"action": "opened","number": 1503,"repository":{"full_name": "tektoncd/triggers", "clone_url": "https://github.com/tektoncd/triggers.git"}, "sender":{"login": "khrm"}}' \
 http://localhost:8080
